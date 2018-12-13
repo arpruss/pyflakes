fdaa170b151f8847f6e91cf5d7025239af9407da
arpruss <arpruss@gmail.com>
2018-12-12 19:49:19 -0600

    opt

--- snowflakes.py
+++ snowflakes.py
@@ -14,44 +14,42 @@ theta = 0.112
 mu = 0.06
 gamma = 0.00006
 sigma = 1e-5
+seed = 1
 
 class HexState(object):
-    def __init__(self,a,b,c,d,bdy):
+    def __init__(self,a,b,c,d,filledNeighbors):
         self.a = a
         self.b = b
         self.c = c 
         self.d = d
-        self.bdy = bdy
+        self.filledNeighbors = filledNeighbors
         
     def __bool__(self):
         return bool(self.a)
         
     def clone(self):
-        return HexState(self.a,self.b,self.c,self.d,self.bdy)
+        return HexState(self.a,self.b,self.c,self.d,self.filledNeighbors)
         
     def __repr__(self):
-        return "(%s,%s,%g,%g,%g)" % (self.a,self.bdy,self.b,self.c,self.d)
+        return "(%s,%g,%g,%g,%d)" % (self.a,self.b,self.c,self.d,self.filledNeighbors)
     
     __nonzero__ = __bool__
     
 def initializer(ri):
     r,i = ri
     if r == 0:
-        return HexState(a=True,b=0,c=1,d=0,bdy=False)
+        return HexState(a=True,b=0,c=1,d=0,filledNeighbors=0)
     elif r == 1:
-        return HexState(a=False,b=0,c=0,d=rho,bdy=True)
+        return HexState(a=False,b=0,c=0,d=rho,filledNeighbors=1)
     else:
-        return HexState(a=False,b=0,c=0,d=rho,bdy=False)
+        return HexState(a=False,b=0,c=0,d=rho,filledNeighbors=0)
         
-def outside(ri):
-    return OUTSIDE
-    
 def evolve():
     # i. Diffusion
     for ri in board.getCoordinates():
         hex = board[ri]
         if not hex.a:
-            if hex.bdy:
+            if hex.filledNeighbors:
                 s = hex.d
                 for y in board.getNeighbors(ri):
                     nhex = board[y]
@@ -70,35 +68,31 @@ def evolve():
             hex.d = scratch[ri]
     
             # ii. Freezing
-            if hex.bdy:
+            if hex.filledNeighbors:
                 hex.b += (1-kappa)*hex.d
                 hex.c += kappa*hex.d
                 hex.d = 0.
                 
     # iii. Attachment
 
-    frozeSomething = False
     for ri in board.getCoordinates():
         hex = board[ri]
-        if hex.bdy:
-            n = board.countFilledNeighbors(ri)
-            if ( ( (n == 1 or n == 2) and hex.b >= beta ) or 
+        if hex.filledNeighbors and not hex.a:
+            n = hex.filledNeighbors
+            if ( ( hex.b >= beta and n <= 2 ) or 
                  ( n == 3 and ( hex.b >= 1 or (hex.b >= alpha and hex.d + sum(board[y].d for y in board.getNeighbors(ri)) < theta) ) ) or
                  n >= 4 ):
                 hex.a = True
                 hex.c += hex.b
                 hex.b = 0.
-                hex.bdy = False
-                frozeSomething = True
+                for y in board.getNeighbors(ri):
+                    board[y].filledNeighbors += 1
                 
+    # iv. Melting
     for ri in board.getCoordinates():
         hex = board[ri]
         if not hex.a:
-            if frozeSomething:
-                hex.bdy = any(board[y].a for y in board.getNeighbors(ri))
-            
-            # iv. Melting
-            if hex.bdy:
+            if hex.filledNeighbors:
                 hex.d += mu * hex.b + gamma * hex.c
                 hex.b *= 1-mu
                 hex.c *= 1-gamma
